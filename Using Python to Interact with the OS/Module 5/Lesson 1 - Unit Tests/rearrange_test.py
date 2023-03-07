f4298b53f9e4e34ec15f19923802adc265a7b7b5
unknown <davidoadeleke90@gmail.com>
2023-03-07 20:24:59 +0100

    Another One

---
+++ Using Python to Interact with the OS/Module 5/Lesson 1 - Unit Tests/rearrange_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from rearrange import rearrange_name
+from unittest import TestCase, main
+
+
+class TestRearrange(TestCase):
+    
+    def test_basic(self):
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Volataire"
+        expected = "Volataire"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+main()
